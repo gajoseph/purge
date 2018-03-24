94558025c81dae9ac6411b6328ffbfed385f0350
TGAJ2 <george_joseph@unigroup.com>
2018-03-23 19:45:26 -0500

    purge code added

--- src/main/java/dbutils/dbtype.java
+++ src/main/java/dbutils/dbtype.java
@@ -18,280 +18,335 @@ public class dbtype {
 
 
 
-public List<jdbcType> db2types = new ArrayList<jdbcType>();
-    
+    public static List<jdbcType> db2types = loadDb2TYpes();
 
-public enum isnull{                
-NULL(1),
-NOT_NULL(2);
 
- private final int isnullCode;
+    public enum isnull{
+        NULL(1),
+        NOT_NULL(2);
 
-    isnull(int levelCode) {
-        this.isnullCode = levelCode;
+        private final int isnullCode;
+
+        isnull(int levelCode) {
+            this.isnullCode = levelCode;
 
 
         }
-private static final Map<String, isnull> lookup = new HashMap<String, isnull>();    
+        private static final Map<String, isnull> lookup = new HashMap<String, isnull>();
 
-static {
-        for (isnull d : isnull.values()) {
-            lookup.put(Integer.toString(d.isnullCode), d);
+        static {
+            for (isnull d : isnull.values()) {
+                lookup.put(Integer.toString(d.isnullCode), d);
+            }
+        }
+
+
+    }
+
+
+
+    public enum db{
+        POSTGRES(2003),
+        DB2(-5),
+        MYSQL(-100)
+        ;
+
+        private final int dbCode;
+
+        db(int levelCode) {
+            this.dbCode = levelCode;
+
+
+        }
+        private static final Map<String, db> lookup = new HashMap<String, db>();
+
+        static {
+            for (db d : db.values()) {
+                lookup.put(Integer.toString(d.dbCode), d);
+            }
+        }
+
+
+
+        public static db get(String abbreviation) {
+            return lookup.get(abbreviation);
         }
     }
 
-    
-}
-    
-    
-    
-public enum db{                
-POSTGRES(2003),
-DB2(-5),
-MYSQL(-100)
-;
+    public enum reference_option {
+        importedKeyNoAction(3), //- do not allow delete of primary key if it has been imported
+        importedKeyCascade(0),// - delete rows that import a deleted key
+        importedKeySetNull(2),// - change imported key to NULL if its primary key has been deleted
+        importedKeyRestrict(1),// - same as importedKeyNoAction (for ODBC 2.x compatibility)
+        importedKeySetDefault(4);// - change imported key to default if its primary key has been deleted
 
- private final int dbCode;
 
-    db(int levelCode) {
-        this.dbCode = levelCode;
+
+        private final int reference_option_code;
+
+        reference_option(int levelCode) {
+            this.reference_option_code = levelCode;
 
 
         }
-private static final Map<String, db> lookup = new HashMap<String, db>();    
+        private static final Map<String, reference_option> lookup = new HashMap<String, reference_option>();
 
-static {
-        for (db d : db.values()) {
-            lookup.put(Integer.toString(d.dbCode), d);
+        static {
+            for (reference_option d : reference_option.values()) {
+                lookup.put(Integer.toString(d.reference_option_code), d);
+            }
         }
-    }
-
-    
-
- public static db get(String abbreviation) {
-        return lookup.get(abbreviation);
-    }
-} 
-
-public enum reference_option {                
-importedKeyNoAction(3), //- do not allow delete of primary key if it has been imported
-importedKeyCascade(0),// - delete rows that import a deleted key
-importedKeySetNull(2),// - change imported key to NULL if its primary key has been deleted
-importedKeyRestrict(1),// - same as importedKeyNoAction (for ODBC 2.x compatibility)
-importedKeySetDefault(4);// - change imported key to default if its primary key has been deleted
- 
-    
-    
- private final int reference_option_code;
-
-     reference_option(int levelCode) {
-        this.reference_option_code = levelCode;
 
 
+
+        public static reference_option get(String abbreviation) {
+            return lookup.get(abbreviation);
         }
-private static final Map<String, reference_option> lookup = new HashMap<String, reference_option>();    
 
-static {
-        for (reference_option d : reference_option.values()) {
-            lookup.put(Integer.toString(d.reference_option_code), d);
-        }
+
     }
 
-    
 
- public static reference_option get(String abbreviation) {
-        return lookup.get(abbreviation);
+
+
+
+    interface JDBC {
+
     }
- 
-  
-} 
 
 
-    
-    
-    
-interface JDBC {
- 
-}    
-    
-    
-    
-    
-public enum lJDBC implements JDBC{                
-    ARRAY(2003),
-    BIGINT(-5),
-    BINARY(-2),
-    BIT(-7),
-    BLOB(2004),
-    BOOLEAN(16),
-    CHAR(1),
-    CLOB(2005),
-    DATALINK(70),
-    DATE(91),
-    DECIMAL(3),
-    DISTINCT(2001),
-    DOUBLE(8),
-    FLOAT(6),
-    INTEGER(4),
-    JAVA_OBJECT(2000),
-    LONGNVARCHAR(-16),
-    LONGVARBINARY(-4),
-    LONGVARCHAR(-1),
-    NCHAR(-15),
-    NCLOB(2011),
-    NULL(0),
-    NUMERIC(2),
-    NVARCHAR(-9),
-    OTHER(1111),
-    REAL(7),
-    REF(2006),
-    ROWID(-8),
-    SMALLINT(5),
-    SQLXML(2009),
-    STRUCT(2002),
-    TIME(92),
-    TIMESTAMP(93),
-    TINYINT(-6),
-    VARBINARY(-3),
-    VARCHAR(12)
-;
 
- private final int lJDBCCode;
 
-    lJDBC(int levelCode) {
-        this.lJDBCCode = levelCode;
+    public enum lJDBC implements JDBC{
+        ARRAY(2003),
+        BIGINT(-5),
+        BINARY(-2),
+        BIT(-7),
+        BLOB(2004),
+        BOOLEAN(16),
+        CHAR(1),
+        CLOB(2005),
+        DATALINK(70),
+        DATE(91),
+        DECIMAL(3),
+        DISTINCT(2001),
+        DOUBLE(8),
+        FLOAT(6),
+        INTEGER(4),
+        JAVA_OBJECT(2000),
+        LONGNVARCHAR(-16),
+        LONGVARBINARY(-4),
+        LONGVARCHAR(-1),
+        NCHAR(-15),
+        NCLOB(2011),
+        NULL(0),
+        NUMERIC(2),
+        NVARCHAR(-9),
+        OTHER(1111),
+        REAL(7),
+        REF(2006),
+        ROWID(-8),
+        SMALLINT(5),
+        SQLXML(2009),
+        STRUCT(2002),
+        TIME(92),
+        TIMESTAMP(93),
+        TINYINT(-6),
+        VARBINARY(-3),
+        VARCHAR(12)
+        ;
+
+        private final int lJDBCCode;
+
+        lJDBC(int levelCode) {
+            this.lJDBCCode = levelCode;
 
 
         }
-private static final Map<String, lJDBC> lookup = new HashMap<String, lJDBC>();    
+        private static final Map<String, lJDBC> lookup = new HashMap<String, lJDBC>();
 
-static {
-        for (lJDBC d : lJDBC.values()) {
-            lookup.put(Integer.toString(d.lJDBCCode), d);
+        static {
+            for (lJDBC d : lJDBC.values()) {
+                lookup.put(Integer.toString(d.lJDBCCode), d);
+            }
+        }
+
+
+
+        public static lJDBC get(String abbreviation) {
+            return lookup.get(abbreviation);
         }
     }
-
-    
-
- public static lJDBC get(String abbreviation) {
-        return lookup.get(abbreviation);
-    }
-} 
 
 /*
-postgres JDBC mapping 
+postgres JDBC mapping
 */
 
 
-public enum PosJDBC implements JDBC{                
-ARRAY(2003),
-BIGINT(-5),
-BYTEA(-2),
-BIT(-7),
-BLOB(2004),
-BOOLEAN(16),
-CHARACTER(1),
-CLOB(2005),
-DATALINK(70),
-DATE(91),
-DECIMAL(3),
-DISTINCT(2001),
-DOUBLE_PRECISION(8),
-FLOAT(6),
-INTEGER(4),
-JAVA_OBJECT(2000),
-TEXT(-16) ,//LONGNVARCHAR(-16),
-BYTE_A(-4), // --LONGVARBINARY(-4),
-LONGVARCHAR(-1),
-NCHAR(-15),
-NCLOB(2011),
-NULL(0),
-NUMERIC(2),
-NVARCHAR(-9),
-OTHER(1111),
-REAL(7),
-REF(2006),
-ROWID(-8),
-SMALLINT(5),
-XML(2009),
-STRUCT(2002),
-TIME(92),
-TIMESTAMP(93),
-TINYINT(-6),
-VARBINARY(-3),
-VARCHAR(12)
-;
+    public enum PosJDBC implements JDBC{
+        ARRAY(2003),
+        BIGINT(-5),
+        BYTEA(-2),
+        BIT(-7),
+        BLOB(2004),
+        BOOLEAN(16),
+        CHARACTER(1),
+        CLOB(2005),
+        DATALINK(70),
+        DATE(91),
+        DECIMAL(3),
+        DISTINCT(2001),
+        DOUBLE_PRECISION(8),
+        FLOAT(6),
+        INTEGER(4),
+        JAVA_OBJECT(2000),
+        TEXT(-16) ,//LONGNVARCHAR(-16),
+        BYTE_A(-4), // --LONGVARBINARY(-4),
+        LONGVARCHAR(-1),
+        NCHAR(-15),
+        NCLOB(2011),
+        NULL(0),
+        NUMERIC(2),
+        NVARCHAR(-9),
+        OTHER(1111),
+        REAL(7),
+        REF(2006),
+        ROWID(-8),
+        SMALLINT(5),
+        XML(2009),
+        STRUCT(2002),
+        TIME(92),
+        TIMESTAMP(93),
+        TINYINT(-6),
+        VARBINARY(-3),
+        VARCHAR(12)
+        ;
 
- private final int PosJDBCCode;
+        private final int PosJDBCCode;
 
-    PosJDBC(int levelCode) {
-        this.PosJDBCCode = levelCode;
+        PosJDBC(int levelCode) {
+            this.PosJDBCCode = levelCode;
 
 
-}
-private static final Map<String, PosJDBC> lookup = new HashMap<String, PosJDBC>();    
+        }
+        private static final Map<String, PosJDBC> lookup = new HashMap<String, PosJDBC>();
 
-static 
-    {
-        for (PosJDBC d : PosJDBC.values()) 
+        static
         {
-            lookup.put(Integer.toString(d.PosJDBCCode), d);
+            for (PosJDBC d : PosJDBC.values())
+            {
+                lookup.put(Integer.toString(d.PosJDBCCode), d);
+            }
+        }
+
+
+
+        public static PosJDBC get(String abbreviation) {
+            return lookup.get(abbreviation);
         }
     }
 
-    
+/// DB2 Mapping
 
- public static PosJDBC get(String abbreviation) {
-        return lookup.get(abbreviation);
+
+
+// MYSQL Mapping
+
+    public enum db2JDBC implements JDBC{
+        LONG_VARCHAR(-1),
+        CHAR(1),
+        DECIMAL(3),
+        INTEGER(4) ,//ok
+        SMALLINT(5) ,//ok
+        REAL(7) ,
+        DOUBLE(8) ,
+        VARCHAR(12) ,
+        BOOLEAN(16	),
+        DATE(91	),
+        TIME(92	),
+        TIMESTAMP(93	),
+        DECFLOAT(1111	),
+        XML(2009	),// remapped to SQLXMLXML				DATA_TYPE: 	1111
+        DISTINCT(2001	),
+        ROW(2002	),
+        ARRAY(2003	),
+        BLOB(2004	),
+        CLOB(2005	),
+        //DBCLOB(2005	),
+        BIGINT(-5	),
+        LONG_VARCHAR_FOR_BIT_DATA(-4	),
+        VARCHAR_FOR_BIT_DATA(-3	),
+        CHAR_FOR_BIT_DATA(-2),
+//    VARCHAR () FOR BIT DATA(2004	),
+//    CHAR () FOR BIT DATA(2004	),
+
+
+        ;
+
+        private final int db2JDBCCode;
+
+        db2JDBC(int levelCode) {
+            this.db2JDBCCode = levelCode;
+
+        }
+        private static final Map<String, db2JDBC> lookup = new HashMap<String, db2JDBC>();
+
+        static {
+            for (db2JDBC d : dbtype.db2JDBC.values()) {
+                lookup.put(Integer.toString(d.db2JDBCCode), d);
+            }
+        }
+        public static db2JDBC get(String abbreviation) {
+            return lookup.get(abbreviation);
+        }
     }
-} 
 
-/// DB2 Mapping 
 
- 
-public void loadDb2TYpes() {
-        
-    db2types.add(new jdbcType("LONG VARGRAPHIC"	,	-1	) );
-    db2types.add(new jdbcType(	"CHAR"          ,	1	) );
-    db2types.add(new jdbcType(	"GRAPHIC"	,	1	) );
-    db2types.add(new jdbcType(	"DECIMAL"	,	3	) );
-    db2types.add(new jdbcType(	"INTEGER"	,	4	) );
-    db2types.add(new jdbcType(	"SMALLINT"	,	5	) );
-    db2types.add(new jdbcType(	"REAL"          ,	7	) );
-    db2types.add(new jdbcType(	"DOUBLE"	,	8	) );
-    db2types.add(new jdbcType(	"VARCHAR"	,	12	) );
-    db2types.add(new jdbcType(	"VARGRAPHIC"	,	12	) );
-    db2types.add(new jdbcType(	"BOOLEAN"	,	16	) );
-    db2types.add(new jdbcType(	"DATE"          ,	91	) );
-    db2types.add(new jdbcType(	"TIME"          ,	92	) );
-    db2types.add(new jdbcType(	"TIMESTAMP"	,	93	) );
-    db2types.add(new jdbcType(	"DECFLOAT"	,	1111	) );
-    db2types.add(new jdbcType(	"XML"           ,	1111	) );
-    db2types.add(new jdbcType(	"DISTINCT"	,	2001	) );
-    db2types.add(new jdbcType(	"ROW"           ,	2002	) );
-    db2types.add(new jdbcType(	"ARRAY"         ,	2003	) );
-    db2types.add(new jdbcType(	"BLOB"          ,	2004	) );
-    db2types.add(new jdbcType(	"CLOB"          ,	2005	) );
-    db2types.add(new jdbcType(	"DBCLOB"	,	2005	) );
 
-    db2types.add(new jdbcType(	"BIGINT"                        ,	-5	) );
-    db2types.add(new jdbcType(	"LONG VARCHAR FOR BIT DATA"	,	2004	) );
-    db2types.add(new jdbcType(	"VARCHAR () FOR BIT DATA"	,	2004	) );
-    db2types.add(new jdbcType(	"CHAR () FOR BIT DATA"          ,	2004	) );
-    db2types.add(new jdbcType(	"LONG VARCHAR"                  ,	-1	) );
-    db2types.add(new jdbcType(	"LONG VARGRAPHIC"               ,	-1	) );
+    public static List<jdbcType>  loadDb2TYpes() {
+        List<jdbcType> db2types = new ArrayList<jdbcType>();
 
-        
-        
-        
-        
-}
+        db2types.add(new jdbcType("LONG VARGRAPHIC"	,	-1	) );
+        db2types.add(new jdbcType(	"CHAR"          ,	1	) );
+//    db2types.add(new jdbcType(	"GRAPHIC"	,	1	) );
+        db2types.add(new jdbcType(	"DECIMAL"	,	3	) );
+        db2types.add(new jdbcType(	"INTEGER"	,	4	) );//ok
+        db2types.add(new jdbcType(	"SMALLINT"	,	5	) );//ok
+        db2types.add(new jdbcType(	"REAL"          ,	7	) );
+        db2types.add(new jdbcType(	"DOUBLE"	,	8	) );
+        db2types.add(new jdbcType(	"VARCHAR"	,	12	) );
+//    db2types.add(new jdbcType(	"VARGRAPHIC"	,	12	) );
+        db2types.add(new jdbcType(	"BOOLEAN"	,	16	) );
+        db2types.add(new jdbcType(	"DATE"          ,	91	) );
+        db2types.add(new jdbcType(	"TIME"          ,	92	) );
+        db2types.add(new jdbcType(	"TIMESTAMP"	,	93	) );
+        db2types.add(new jdbcType(	"DECFLOAT"	,	1111	) );
+        db2types.add(new jdbcType(	"XML"           ,	2009	) );
+        db2types.add(new jdbcType(	"DISTINCT"	,	2001	) );
+        db2types.add(new jdbcType(	"ROW"           ,	2002	) );
+        db2types.add(new jdbcType(	"ARRAY"         ,	2003	) );
+        db2types.add(new jdbcType(	"BLOB"          ,	2004	) );
+        db2types.add(new jdbcType(	"CLOB"          ,	2005	) );
+        db2types.add(new jdbcType(	"DBCLOB"	,	2005	) );
+
+        db2types.add(new jdbcType(	"BIGINT"                        ,	-5	) );
+        db2types.add(new jdbcType(	"LONG VARCHAR FOR BIT DATA"	,	2004	) );
+        db2types.add(new jdbcType(	"VARCHAR () FOR BIT DATA"	,	2004	) );
+        db2types.add(new jdbcType(	"CHAR () FOR BIT DATA"          ,	2004	) );
+        db2types.add(new jdbcType(	"LONG VARCHAR"                  ,	-1	) );
+        db2types.add(new jdbcType(	"LONG VARGRAPHIC"               ,	-1	) );
+
+        return db2types;
+
+
+
+    }
 
 // MYSQL Mapping
 
     public enum mySqlJDBC implements JDBC{
         ARRAY(2003),
-                BIGINT(-5), // mysql has
+        BIGINT(-5), // mysql has
         LONGBLOB_(-2), // mysql BINARY
         BIT(-7),
         LONGBLOB(2004),
@@ -302,9 +357,9 @@ public void loadDb2TYpes() {
         DATE(91),
         DECIMAL(3),// mysql has
         REAL_(2001),// mysql has docuble precision but mappin gto REAL
-            DOUBLE(8),
+        DOUBLE(8),
         FLOAT(6),// has float as well
-                INT(4), //mysql has only inT INTEGER(4),
+        INT(4), //mysql has only inT INTEGER(4),
         JAVA_OBJECT(2000),
         //LONGNVARCHAR(-16),
         LONGTEXT(-16),
@@ -316,15 +371,15 @@ public void loadDb2TYpes() {
         DECIMAL_(2),// mysql deons't have mueric to mappying to decimal
         VARCHAR_1(-9),
         OTHER(1111),
-            REAL(7),//mysql has REAL
+        REAL(7),//mysql has REAL
         REF(2006),
         ROWID(-8),
-                SMALLINT(5), // mysql has
+        SMALLINT(5), // mysql has
         SQLXML(2009),
         STRUCT(2002),
         TIME(92),
         DATETIME(93),// mysql TIMESTAMP has a range of '1970-01-01 00:00:01' UTC to '2038-01-19 03:14:07' UTC. so chnaging to Datetime
-                TINYINT(-6), //mysql has tinyint
+        TINYINT(-6), //mysql has tinyint
         VARBINARY(-3), // mysql VARBINARY
         VARCHAR(12)
         ;
--- src/main/java/dbutils/iDataloc.java
+++ src/main/java/dbutils/iDataloc.java
@@ -9,6 +9,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import static dbutils.idrive.lSumBJCLogger;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,16 +19,16 @@ import static dbutils.idrive.lSumBJCLogger;
  */
 public class iDataloc extends tfield {
     public Connection _conn;
-    public String surl = "";// JDBC connection URL 
+    public String surl = "";// JDBC connection URL
 
-    
-    
-    
-    
+
+
+
+
     public iDataloc(String url, String class4Name, String db2user, String db2pass) {
         try {
             Class.forName(class4Name);
-            
+
             _conn =java.sql.DriverManager.getConnection(url, db2user, db2pass);
             _conn.setAutoCommit(false);
             lSumBJCLogger.WriteLog(" Created connection  " + url);
@@ -39,50 +42,88 @@ public class iDataloc extends tfield {
 
             lSumBJCLogger.WriteErrorStack("Error ", e);
 
-        } 
-        
+        }
+
         catch (ClassNotFoundException E) {
-             lSumBJCLogger.WriteErrorStack(url, E);
+            lSumBJCLogger.WriteErrorStack(url, E);
 
 
-        } 
-        
-        finally 
+        }
+
+        finally
         {
 
         }
 
     }
-    
+
+    public void iprintDbtypes(){
+
+        ResultSet rsColumns = null;
+        String sPrintHeader = "";
+        int sPrintHeaderLen= 0;
+
+
+        sPrintHeader = sPrintHeader+ String.format("\n|%50s | %10s |"
+                , "Type Name", "Data_type");
+        sPrintHeaderLen = sPrintHeader.length();
+        sPrintHeader = lSumBJCLogger.sTabPrint(sPrintHeader, sPrintHeaderLen, "-", "|");
+
+        try {
+            rsColumns = _conn.getMetaData()
+                    .getTypeInfo();
+            lSumBJCLogger.setSYSTEM_LOG_OUT(true);
+
+            while (rsColumns.next()) {
+                sPrintHeader= sPrintHeader
+                        + String.format("\n|%50s | %10s |"
+                        ,rsColumns.getString("TYPE_NAME")
+                        ,rsColumns.getString("DATA_TYPE")  );
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(iDataloc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            sPrintHeader = sPrintHeader + lSumBJCLogger.printLine( sPrintHeaderLen, "-", "|");
+            lSumBJCLogger.WriteLog(sPrintHeader);
+            lSumBJCLogger.setSYSTEM_LOG_OUT(false);
+
+        }
+
+    }
+
+
     public void iprintDbinfo(String strHeaderInfo) throws SQLException{
         lSumBJCLogger.setSYSTEM_LOG_OUT(true);
-        lSumBJCLogger.WriteLog("/*============================================================================");  
+        lSumBJCLogger.WriteLog("/*============================================================================");
         lSumBJCLogger.WriteLog("-- " + strHeaderInfo+ " Db:");
         lSumBJCLogger.WriteLog("============================================================================*/");
 
-        
-        lSumBJCLogger.WriteLog("DatabaseProductName: " + _conn.getMetaData().getDatabaseProductName() );  
-        lSumBJCLogger.WriteLog("DatabaseProductVersion: " + _conn.getMetaData().getDatabaseProductVersion() );  
-        lSumBJCLogger.WriteLog("DatabaseMajorVersion: " + _conn.getMetaData().getDatabaseMajorVersion() );  
-        lSumBJCLogger.WriteLog("DatabaseMinorVersion: " +_conn.getMetaData().getDatabaseMinorVersion() );  
-         lSumBJCLogger.WriteLog("=====  Driver info =====");  
-         lSumBJCLogger.WriteLog("DriverName: " + _conn.getMetaData().getDriverName() );  
-         lSumBJCLogger.WriteLog("DriverVersion: " + _conn.getMetaData().getDriverVersion() );  
-         lSumBJCLogger.WriteLog("DriverMajorVersion: " + _conn.getMetaData().getDriverMajorVersion() );  
-         lSumBJCLogger.WriteLog("DriverMinorVersion: " + _conn.getMetaData().getDriverMinorVersion() );  
-         lSumBJCLogger.setSYSTEM_LOG_OUT(false);
-    
+
+        lSumBJCLogger.WriteLog("DatabaseProductName: " + _conn.getMetaData().getDatabaseProductName() );
+        lSumBJCLogger.WriteLog("DatabaseProductVersion: " + _conn.getMetaData().getDatabaseProductVersion() );
+        lSumBJCLogger.WriteLog("DatabaseMajorVersion: " + _conn.getMetaData().getDatabaseMajorVersion() );
+        lSumBJCLogger.WriteLog("DatabaseMinorVersion: " +_conn.getMetaData().getDatabaseMinorVersion() );
+        lSumBJCLogger.WriteLog("=====  Driver info =====");
+        lSumBJCLogger.WriteLog("DriverName: " + _conn.getMetaData().getDriverName() );
+        lSumBJCLogger.WriteLog("DriverVersion: " + _conn.getMetaData().getDriverVersion() );
+        lSumBJCLogger.WriteLog("DriverMajorVersion: " + _conn.getMetaData().getDriverMajorVersion() );
+        lSumBJCLogger.WriteLog("DriverMinorVersion: " + _conn.getMetaData().getDriverMinorVersion() );
+        lSumBJCLogger.setSYSTEM_LOG_OUT(false);
+
     }
-            
-            
-            
-    
+
+
+
+
 
     protected void finalize() throws Throwable {
         tfield l;
         String indx1;
         try {
-            if (!(_conn == null)) 
+            if (!(_conn == null))
             {
                 if (!_conn.isClosed()) {
                     _conn.close();
